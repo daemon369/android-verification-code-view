588524f7befab0de224ee9d343980a18b4112fcb
daemon369 <daemon336699@gmail.com>
2019-06-25 16:36:29 +0800

    new android project

---
+++ app/src/main/java/me/daemon/verificationcode/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.daemon.verificationcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
