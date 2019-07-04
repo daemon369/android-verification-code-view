65e9623bb8cb9c65e862a09eeb8f4b14c62a85ab
daemon369 <daemon336699@gmail.com>
2019-07-04 21:27:23 +0800

    using daemon_vc_gridDividerSize to set divider size

--- app/src/main/java/me/daemon/verificationcode/demo/MainActivity.kt
+++ app/src/main/java/me/daemon/verificationcode/demo/MainActivity.kt
@@ -3,10 +3,11 @@ package me.daemon.verificationcode.demo
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
+import android.util.Log.i
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import me.daemon.verificationcode.VerificationCodeView
+import me.daemon.view.common.dp2px
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,18 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        vc.DRAW_AUXILIARY_LINE = true
+        vc.capacity = 6
         vc.gridDividerSize = 10
         vc.gridBackground = ColorDrawable(Color.YELLOW)
+        vc.textSize = dp2px(30f)
+        vc.textColor = Color.CYAN
         vc.listener = object : VerificationCodeView.Listener {
             override fun onChanged(
                 view: VerificationCodeView,
                 content: String,
                 isFullFilled: Boolean
             ) {
-                Log.i(
+                i(
                     MainActivity::class.java.name,
                     "on verification code view changed: $view, $content, $isFullFilled"
+                )
+            }
+        }
+
+        vc2.DRAW_AUXILIARY_LINE = true
+        vc2.listener = object : VerificationCodeView.Listener {
+            override fun onChanged(
+                view: VerificationCodeView,
+                content: String,
+                isFullFilled: Boolean
+            ) {
+                i(
+                    MainActivity::class.java.name,
+                    "on verification code view changed 2: $view, $content, $isFullFilled"
                 )
             }
         }
