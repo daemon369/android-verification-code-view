8e6ed6165b818741966977c37522c5bed2cfdeb3
daemon369 <daemon336699@gmail.com>
2020-06-28 17:02:03 +0800

    add cursor support

---
+++ app/src/main/java/me/daemon/verificationcode/demo/App.kt
@@ -0,0 +1,12 @@
+package me.daemon.verificationcode.demo
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+//        android.os.Debug.waitForDebugger()
+    }
+
+}
